80d56f15791b8a050f03f5e230b89bdd5d7f2fc3
Priya-150 <159451684+Priya-150@users.noreply.github.com>
2025-09-10 09:17:04 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import Firebase SDKs
+// Add Firebase CDN links in <head> if not using modules
+// Example: <script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js"></script>
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAkH6er6KHYs_wGpNsVt7vePd9_Fx67Pto",
+  authDomain: "login-2bd71.firebaseapp.com",
+  projectId: "login-2bd71",
+  storageBucket: "login-2bd71.firebasestorage.app",
+  messagingSenderId: "685912637469",
+  appId: "1:685912637469:web:cb5023e5e89338750506f4",
+  measurementId: "G-92Z5R7RKSY"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
